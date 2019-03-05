43de8e16a0acdac8b7bd3fd2fad651e353de0bb7
marialauramirabelli <mlm809@nyu.edu>
2019-03-05 09:00:37 +0400

    Update client.js

--- Connected-Device/simpleConnectedDeviceAssignment/public/client.js
+++ Connected-Device/simpleConnectedDeviceAssignment/public/client.js
@@ -2,12 +2,12 @@
 function signIn() {
     //New user needs to enter information, primarly email address and password
     //How do I match up a humidity sensor with a specific user?
-    //Info saved on a JSON file?
+    //Info saved on a JSON or txt file?
 }
 
 function logIn() {
     //Existing user needs to enter email address and password
-    //Info verified by comparing it to info saved on a JSON file?
+    //Info verified by comparing it to info saved on a JSON or txt file?
 }
 
 function showReading(){
