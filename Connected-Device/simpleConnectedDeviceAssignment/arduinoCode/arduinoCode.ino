9bf1edd5473782dc7a4719f61d167dd1dd67281b
marialauramirabelli <mlm809@nyu.edu>
2019-03-05 10:29:47 +0400

    Add files via upload

---
+++ Connected-Device/simpleConnectedDeviceAssignment/arduinoCode/arduinoCode.ino
@@ -0,0 +1,116 @@
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <WiFiUdp.h>
+
+int status = WL_IDLE_STATUS;
+#include "arduino_secrets.h"
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+unsigned int localPort = 5000;      // local port to listen on
+
+char packetBuffer[255]; //buffer to hold incoming packet
+
+//If each element in the packetBuffer is 1 byte, is the maximum analog reading I can send 255? Does this mean I have to map the input
+//values to the 0-255 range?
+
+WiFiUDP Udp;
+
+const int analogInput = A0;
+//const int output = pin for an actuator, either analog or digital, depending on what I'll use.
+//int minimumHumidity = threshold for humidity, when below this number, send alert/reminder
+//int response = response from the server for an actuator
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  // attempt to connect to WiFi network:
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  Serial.println("Connected to wifi");
+  printWiFiStatus();
+
+  Serial.print("Initializing WiFiUDP library and listening on port ");
+  Serial.println(localPort);
+  Udp.begin(localPort);
+}
+
+void loop() {
+
+  // IP address of the receiving device
+  IPAddress receivingDeviceAddress(192, 168, 1, 15);
+  unsigned int receivingDevicePort = 2390;
+
+  Udp.beginPacket(receivingDeviceAddress, receivingDevicePort);
+
+  Udp.write(analogInput);
+  //might possibly need to map analogInput first
+  
+  //if(analogInput < minimumHumidity){
+    //let server know that alert/reminder must be sent?
+    //or should the server interpret this itself?
+  //}
+  
+  Udp.endPacket();
+
+  // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+  if (packetSize)
+  {
+    if (packetSize != 3){
+      Serial.println("Incorrect packet size received.");
+    }
+    else{
+      Serial.print("Received packet of size ");
+      Serial.println(packetSize);
+      Serial.print("From ");
+      IPAddress remoteIp = Udp.remoteIP();
+      Serial.print(remoteIp);
+      Serial.print(", port ");
+      Serial.println(Udp.remotePort());
+  
+      // read the packet into packetBufffer
+      int len = Udp.read(packetBuffer, 255);
+      //digitalWrite(response, packetBuffer[0]);
+      //used for the actuator, I'm not sure what that will be yet
+    }
+  }
+}
+
+
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("My IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
